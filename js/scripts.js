65f7e672eb576be6cbeaab371cf4498437ec66a7
Chrispus Ngure <chrispus.ngure@gmail.com>
2018-12-04 12:57:00 +0300

    edit function

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 var triangle = function () {
-  var sideA = parseInt("input#sideA");
-  var sideA = parseInt("input#sideB");
-  var sideA = parseInt("input#sideC");
+  var sideA = parseFloat(document.getElementById("sideA").value);
+  var sideB = parseFloat(document.getElementById("sideB").value);
+  var sideC = parseFloat(document.getElementById("sideC").value);
+
   if (sideA === sideB && sideB === sideC && sideC === sideA) {
-    alert("Equilateral triangle")
+    alert("Equilateral triangle");
   } else if (sideA === sideB || sideA === sideC || sideB === sideC) {
-    alert("Isosceles Triangle")
+    alert("Isosceles Triangle");
   } else if (sideA !=== sideB && sideB !=== sideC) {
-    alert("Scalene Triangle")
-  } else if ((sideA+sideB)<=sideC) || (sideB+sideC)<=sideA || (sideA+sideC)<=sideB){
-    alert("Not a Triangle")
+    alert("Scalene Triangle");
+  } else
+    alert("Not a Triangle");
   }
 }

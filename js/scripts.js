f60807a47c2a2fc9b78c749b33ebbedf56ffad50
Chrispus Ngure <chrispus.ngure@gmail.com>
2018-12-06 09:33:52 +0300

    edit function to return Put a Number

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
 var triangle = ["sideA", "sideB", "sideC"]
-var triangle = function () {
+var triangle = function() {
   var sideA = parseInt(document.getElementById("sideA").value);
   var sideB = parseInt(document.getElementById("sideB").value);
   var sideC = parseInt(document.getElementById("sideC").value);
 
-  if (sideA === sideB && sideB === sideC && sideC === sideA) {
+  if ((sideA + sideB) > sideC && (sideA + sideC) > sideB && (sideB + sideC) > sideA) {
+    if (sideA === sideB && sideB === sideC && sideC === sideA) {
       alert("Equilateral triangle");
-  } else if (sideA === sideB || sideA === sideC || sideB === sideC) {
+    } else if (sideA === sideB || sideA === sideC || sideB === sideC) {
       alert("Isosceles Triangle");
-  } else if ((sideA+sideB) <=sideC ||(sideB+sideC)<=sideA || (sideA+sideC)<=sideB) {
-      alert("Not a Triangle");
+    } else {
+      alert("Scalene Triangle");
+    }
+  } else if (isNaN(sideA) || isNaN(sideB) || isNaN(sideC)) {
+    alert("Put a Number")
   } else {
-    alert("Scalene Triangle");
+    alert("Cannot form a Triangle");
   }
 }
